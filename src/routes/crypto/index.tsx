e4eaf075854005f99c0c07dca6886d06439e51de
hyperst8 <huypn82@gmail.com>
2025-05-07 14:20:41 +0200

    Change value

--- src/routes/crypto/index.tsx
+++ src/routes/crypto/index.tsx
@@ -22,7 +22,7 @@ function CryptoLayout() {
     gcTime: 0,
     queryFn: async () => {
       const response = await fetch(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1"
       );
 
       return response.json();
