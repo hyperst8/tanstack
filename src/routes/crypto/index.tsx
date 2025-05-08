6f7e657f1e66f57699d9e28df29888d5adae5782
hyperst8 <huypn82@gmail.com>
2025-05-08 10:14:46 +0200

    Improve typescript and add table sorting

--- src/routes/crypto/index.tsx
+++ src/routes/crypto/index.tsx
@@ -5,6 +5,7 @@ import {
   getCoreRowModel,
   flexRender,
   createColumnHelper,
+  getSortedRowModel,
 } from "@tanstack/react-table";
 import type { CoinData } from "../../types/cryptoTypes";
 
@@ -17,7 +18,11 @@ const columnHelper = createColumnHelper<CoinData>();
 function CryptoLayout() {
   const navigate = useNavigate();
 
-  const { data, error, isPending } = useQuery({
+  const {
+    data = [],
+    error,
+    isLoading,
+  } = useQuery<CoinData[], Error>({
     queryKey: ["coins"],
     gcTime: 0,
     queryFn: async () => {
@@ -25,16 +30,39 @@ function CryptoLayout() {
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1"
       );
 
+      if (!response.ok) throw new Error("Failed to fetch crypto data.");
+
       return response.json();
     },
+    select: (data) =>
+      data.map(
+        ({
+          id,
+          market_cap_rank,
+          name,
+          image,
+          current_price,
+          price_change_percentage_24h,
+        }) => ({
+          id,
+          market_cap_rank,
+          name,
+          image,
+          current_price,
+          price_change_percentage_24h,
+        })
+      ),
   });
 
   const columns = [
-    columnHelper.accessor("market_cap_rank", { header: "Rank" }),
+    columnHelper.accessor("market_cap_rank", {
+      header: "Rank",
+      sortingFn: "basic",
+    }),
     columnHelper.accessor("name", {
       header: "Coin",
       cell: (info) => {
-        const coin = info.row.original as CoinData;
+        const coin = info.row.original;
         return (
           <div className="flex items-center gap-2">
             <img src={coin.image} alt={coin.name} className="w-6 h-6" />
@@ -46,10 +74,12 @@ function CryptoLayout() {
     columnHelper.accessor("current_price", {
       header: "Price",
       cell: (info) => `$${(info.getValue() as number).toFixed(2)}`,
+      sortingFn: "basic",
     }),
     columnHelper.accessor("price_change_percentage_24h", {
       header: "Last 24h",
       cell: (info) => `${(info.getValue() as number).toFixed(2)}%`,
+      sortingFn: "basic",
     }),
   ];
 
@@ -57,9 +87,10 @@ function CryptoLayout() {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
 
-  if (isPending) {
+  if (isLoading) {
     return <div className="min-h-incl-header center-container">Loading...</div>;
   }
 
@@ -76,15 +107,24 @@ function CryptoLayout() {
       <h1 className="text-2xl">Crypto currencies</h1>
       <div className="overflow-x-auto w-full max-w-4xl">
         <table className="min-w-full bg-transparent border rounded-md shadow-md mt-4">
-          <thead className="bg-blue-400 text-white">
+          <thead className="bg-gray-600 text-white">
             {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <th key={header.id} className="p-2 text-left border-b">
+                  <th
+                    key={header.id}
+                    className="p-2 text-left border-b cursor-pointer"
+                    onClick={() => header.column.toggleSorting()}
+                  >
                     {flexRender(
                       header.column.columnDef.header,
                       header.getContext()
                     )}
+                    {header.column.getIsSorted() === "asc"
+                      ? " 🔼"
+                      : header.column.getIsSorted() === "desc"
+                        ? " 🔽"
+                        : ""}
                   </th>
                 ))}
               </tr>
