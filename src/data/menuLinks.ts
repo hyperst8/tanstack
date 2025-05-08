f89699720810cdc8b8ba6e689bb369206a1c9ce1
hyperst8 <huypn82@gmail.com>
2025-05-08 14:43:25 +0200

    Add separate menuLinks

---
+++ src/data/menuLinks.ts
@@ -0,0 +1,6 @@
+export const homepageLinks = [
+  {
+    label: "Crypto currencies",
+    url: "/crypto",
+  },
+];
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { homepageLinks } from "../data/menuLinks";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -9,9 +10,15 @@ function Index() {
     <div className="min-h-incl-header flex flex-col items-center justify-center">
       <h1 className="text-2xl">Welcome!</h1>
       <div className="flex flex-col gap-2 my-4 p-4 border rounded-md shadow-md bg-transparent">
-        <Link to="/crypto" className="home-links text-white bg-green-500">
-          Crypto currencies
-        </Link>
+        {homepageLinks.map((link, index) => (
+          <Link
+            key={index}
+            to={link.url}
+            className="home-links text-white bg-green-500"
+          >
+            {link.label}
+          </Link>
+        ))}
       </div>
     </div>
   );
